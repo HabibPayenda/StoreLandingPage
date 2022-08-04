45aaffa89c2aa151b3a8e70824ce7c38bd7a0585
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-08-04 21:20:53 +0430

    some html and css added

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// const products = [
+//   {
+//     id: 1,
+//     name: "What's your goal?",
+//     pic: 'url("https://media.bodyandfit.com/i/bodyandfit/Whats-your-goal?w=256&sm=aspect&aspect=1:1&fmt=webp")',
+//     items: [
+//       {
+//         itemId: 1,
+//         name: 'itemOne',
+//         usage: 'For Performance Energy and Focus',
+//         price: '2000 Afg',
+//         pic: 'url("https://media.bodyandfit.com/i/bodyandfit/Whats-your-goal?w=256&sm=aspect&aspect=1:1&fmt=webp")',
+//         details: {
+//           desc: 'the descreption of the producd',
+//           benifits: 'benifits of the product',
+//           sugestedUse: 'sugested use of the product',
+//           nutritionalInformation: 'nutritional information about the product',
+//           summary: [
+//             'For Perfomance, Energy and Focus',
+//             '175 mg Caffeine per Serving From Natural Sources',
+//             '3 Grams of Creatine Monohdrate Per Serving',
+//             '1.5 Grams of Beta Alanine Per Serving',
+//           ],
+//           flavor: [
+//           ],
+//         },
+//       },
+//     ],
+//   },
+// ];
